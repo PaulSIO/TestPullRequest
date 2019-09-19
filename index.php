d451afe1228afd8fb30e64d59e39aab2019f4176
stixomat <mathieusoufflard@gmail.com>
2019-09-19 15:08:50 +0200

    la flem

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE php>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <li><a href="Table.php ?num=1">Table de 1</a></li>
+    <li><a href="Table.php ?num=2">Table de 2</a></li>
+    <li><a href="Table.php ?num=3">Table de 3</a></li>
+    <li><a href="Table.php ?num=4">Table de 4</a></li>
+    <li><a href="Table.php ?num=5">Table de 5</a></li>
+    <li><a href="Table.php ?num=6">Table de 6</a></li>
+    <li><a href="Table.php ?num=7">Table de 7</a></li>
+    <li><a href="Table.php ?num=8">Table de 8</a></li>
+    <li><a href="Table.php ?num=9">Table de 9</a></li>
+    <li><a href="Table.php ?num=10">Table de 10</a></li>
+    </ul>    
+</body>
+</html>
